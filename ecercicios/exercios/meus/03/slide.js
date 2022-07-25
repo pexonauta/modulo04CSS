0235fbce1b1544b68b075e5ae03f2dde4cd8a61c
pexonauta <pexofec123@gmail.com>
2022-07-25 18:02:52 -0400

    att do quiz - slideshow

---
+++ ecercicios/exercios/meus/03/slide.js
@@ -0,0 +1,27 @@
+let imgs = [
+    "img/slidesho1.jpg",
+    "img/slidesho2.jpg",
+    "img/slidesho3.jpg",
+    "img/slidesho4.jpg",
+]
+
+let time = 5000, contador = 0, max = imgs.length
+
+function trocarImagem () {
+    document.body.style.backgroundImage = `URL(${imgs[contador]})`
+    contador++
+
+    if(contador >= max) {
+        contador = 0
+    }
+
+
+}
+
+function start () {
+    setInterval(() => {
+        trocarImagem()
+    }, time)
+}
+
+window.addEventListener('load', start)
